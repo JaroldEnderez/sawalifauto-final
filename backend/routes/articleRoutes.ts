70a9984eb00a523c349ed7c113fcc2604e11bb54
unknown <jaroldenderez69@gmail.com>
2025-10-16 19:01:49 +0800

    log tests in article route

--- backend/routes/articleRoutes.ts
+++ backend/routes/articleRoutes.ts
@@ -1,19 +1,23 @@
 import express from 'express'
 import { ArticleModel } from '../models/ArticleModel'
+import mongoose from 'mongoose';
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    try{
-        const {category} = req.query
-        const query = category ? { category } : {}
-        const articles = await ArticleModel.find(query).sort({publishedAt: -1})
-        console.log("Articles found:", articles.length);
-        res.json({success:true, articles})
-    }catch(error){
-        console.error("❌ Error fetching articles: ", error)
-        res.status(500).json({error: "Failed to fetch articles"})
+    try {
+      const { category } = req.query
+      const query = category ? { category } : {}
+      console.log("Incoming request:", req.query);
+      console.log("Mongo connected:", mongoose.connection.readyState); // 1 = connected
+      const articles = await ArticleModel.find(query).sort({ publishedAt: -1 });
+      console.log("Articles found:", articles.length);
+      res.json({ success: true, articles });
+    } catch (error) {
+      console.error("❌ Error fetching articles:", error);
+      res.status(500).json({ error: "Failed to fetch articles" });
     }
-})
+  });
+  
 
 export default router
